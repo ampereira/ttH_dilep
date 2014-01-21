c8afc25b2d0a1e1bb303062ecd7ec605d5fc8eb7
ampereira <lazeroth89@gmail.com>
2014-01-21 16:10:59 +0000

    ds

--- src/dilep_input.cxx
+++ src/dilep_input.cxx
@@ -75,7 +75,6 @@ DilepInput::DilepInput (TLorentzVector _z_lep, TLorentzVector _c_lep, TLorentzVe
 	event_id = _event_id;
 
 	#ifdef D_DICE
-		cout << "TAU" << endl;
 		//hasSolution = new int [dilep_iterations];
 		result 		= new vector<myvector> [dilep_iterations];
 	#else
--- src/dilep_input.h
+++ src/dilep_input.h
@@ -102,7 +102,7 @@ public:
 		inline vector<myvector> getResult (unsigned index) const { return result[index]; }
 
 		/*inline*/ void setHasSol (int x, unsigned index) { hasSolution[index] = x; }
-		/*inline*/ void setResult (vector<myvector> *x, unsigned index) { result[index].assign(x->begin(), x->end()); }
+		/*inline*/ void setResult (vector<myvector> *x, unsigned index) { result[index] = *x; } //.assign(x->begin(), x->end()); }
 	#else
 		inline int getHasSol (void) const { return hasSolution; }
 		inline vector<myvector> getResult (void) const { return result; }
