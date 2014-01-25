664e0b8d6c7cf13e3f4c646feca962d7f8a7a904
ampereira <lazeroth89@gmail.com>
2014-01-25 12:36:15 +0000

    blah

--- src/neut_dice/dilep.h
+++ src/neut_dice/dilep.h
@@ -16,7 +16,7 @@ using namespace std;
 #define STRIDE2(array,i) array[tid * 2 + i]
 #define STRIDE5(array,i) array[tid * 5 + i]
 
-#define BL_SIZE 4451904		// for 4 dilep iterations
+#define BL_SIZE4 4000000		// for 4 dilep iterations
 
 typedef double double_t;
 
@@ -37,8 +37,8 @@ public:
 	 *
 	 */
 
-	double_t _z_bl[BL_SIZE];
-	double_t _c_bl[BL_SIZE];
+	double_t _z_bl[BL_SIZE4];
+	double_t _c_bl[BL_SIZE4];
 
 
 	// Output and control parameters
