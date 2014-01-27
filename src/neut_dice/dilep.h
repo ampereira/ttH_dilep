f5b7f34555deef79ee61d8fbb64740b123535a1d
ampereira <lazeroth89@gmail.com>
2014-01-27 16:21:53 +0000

    blah

--- src/neut_dice/dilep.h
+++ src/neut_dice/dilep.h
@@ -5,7 +5,7 @@
 //#include <config/common.h>
 //#include <gamalib/gamalib.h>
 
-#define DILEP_ITERATIONS 1
+#define DILEP_ITERATIONS 16
 
 #include <iostream>
 using namespace std;
@@ -25,7 +25,7 @@ using namespace std;
 #define BL_SIZE2    950000		// for 2 dilep iterations
 #define BL_SIZE4    4000000		// for 4 dilep iterations
 // --== Ate aqui ta testado == --
-#define BL_SIZE16   1000000		// for 16 dilep iterations
+#define BL_SIZE16   10000000		// for 16 dilep iterations
 #define BL_SIZE32   1000000		// for 32 dilep iterations
 #define BL_SIZE64   1000000		// for 64 dilep iterations
 #define BL_SIZE128  1000000		// for 128 dilep iterations
@@ -47,8 +47,8 @@ public:
 	//double_t *_z_bl, *_c_bl;
 
 
-	double_t _z_bl[BL_SIZE1];
-	double_t _c_bl[BL_SIZE1];
+	double_t _z_bl[BL_SIZE16];
+	double_t _c_bl[BL_SIZE16];
 
 
 	// Output and control parameters
