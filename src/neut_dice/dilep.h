52bf9c76f8e28f401641f90af5fb82e1845cd02f
ampereira <lazeroth89@gmail.com>
2014-01-27 16:25:00 +0000

    blah

--- src/neut_dice/dilep.h
+++ src/neut_dice/dilep.h
@@ -25,7 +25,7 @@ using namespace std;
 #define BL_SIZE2    950000		// for 2 dilep iterations
 #define BL_SIZE4    4000000		// for 4 dilep iterations
 // --== Ate aqui ta testado == --
-#define BL_SIZE16   10000000		// for 16 dilep iterations
+#define BL_SIZE16   20000000		// for 16 dilep iterations
 #define BL_SIZE32   1000000		// for 32 dilep iterations
 #define BL_SIZE64   1000000		// for 64 dilep iterations
 #define BL_SIZE128  1000000		// for 128 dilep iterations
