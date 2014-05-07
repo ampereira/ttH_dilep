30aec3f5f152dd4b694f19143f3fae62075bc28d
ampereira <lazeroth89@gmail.com>
2014-05-07 16:51:58 +0100

    dsa

--- src/ttH_dilep.cxx
+++ src/ttH_dilep.cxx
@@ -5243,8 +5243,7 @@ void ttH_dilep::PostLoopCalculations(){
         // =========================================================
         // ===== AO 8 Oct 2010 ===================== above =========
         // =========================================================
-        //if ( ( isData == 0 ) && ( events[0].myRunNumber ==105200 ) ){
-        if ( ( isData == 0 ) && ( myRunNumber ==105200 ) ){
+        if ( ( isData == 0 ) && ( events[0].myRunNumber ==105200 ) ){
 
                 cout << "    " << endl;
                 cout << "    " << endl;
