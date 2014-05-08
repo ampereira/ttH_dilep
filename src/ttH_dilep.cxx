15d445a2f7463be5a263e6e87b33c7abcc096755
ampereira <lazeroth89@gmail.com>
2014-05-08 11:13:41 +0100

    ds

--- src/ttH_dilep.cxx
+++ src/ttH_dilep.cxx
@@ -4663,7 +4663,6 @@ void ttH_dilep::ttDilepKinFit(){
 	    double fac_j1j2H_ttbar;
 	    double mass_j1H_ttbar;
 	    double mass_j2H_ttbar;
-	    DilepInput di;
 	    #pragma omp for schedule(dynamic) nowait
 	    for (Event::event_counter = 0; Event::event_counter < events.size(); ++Event::event_counter){
 	    	
@@ -4725,6 +4724,7 @@ void ttH_dilep::ttDilepKinFit(){
 			#ifdef D_DICE
 			for (unsigned partial_counter = 0; partial_counter < num_Combs * dilep_iterations; ++partial_counter) {
 
+        DilepInput di;
 				float task_id = (float) partial_counter / (float) dilep_iterations;
 				unsigned current_variation = partial_counter % dilep_iterations;
 
@@ -4743,6 +4743,7 @@ void ttH_dilep::ttDilepKinFit(){
 			#else
 		    for (unsigned partial_counter = 0; partial_counter < num_Combs; ++partial_counter, ++total_counter) {
 				
+        DilepInput di;
 
 		    	//task_id = (float) counter / (float) dilep_iterations;
 
